71a64fc6b20d947905346e6d67ec6edc595f49fc
Juan Altmayer Pizzorno <juan@altmayer.com>
2022-08-12 16:29:31 -0400

    - skipped same tests in 3.11.0rc1 as in 3.11.0b4, as they are brittle;

--- tests/slipcover_test.py
+++ tests/slipcover_test.py
@@ -563,7 +563,7 @@ def gen_test_sequence():
     return [(64*1024*arg)//b.arg() for arg in [0xFF, 0xFFFF]]#, 0xFFFFFF]]
 
 
-@pytest.mark.skipif(sys.version.split()[0] == '3.11.0b4', reason='brittle test')
+@pytest.mark.skipif(sys.version.split()[0] in ['3.11.0b4', '3.11.0rc1'], reason='brittle test')
 @pytest.mark.parametrize("N", gen_test_sequence())
 def test_instrument_long_jump(N):
     sci = sc.Slipcover()
