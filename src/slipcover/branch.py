d2e53a318b9c04139bfaf44d4db2b8a1988acf42
Juan Altmayer Pizzorno <juan@altmayer.com>
2024-05-27 15:26:41 -0400

    - changed to use a constant Expr to mark branches with Python >= 3.12,   for which the Python compiler just generates a NOP, speeding up   branch coverage;

--- src/slipcover/branch.py
+++ src/slipcover/branch.py
@@ -26,34 +26,37 @@ def preinstrument(tree: ast.AST) -> ast.AST:
             pass
 
         def _mark_branch(self, from_line: int, to_line: int) -> List[ast.stmt]:
-            mark = ast.Assign([ast.Name(BRANCH_NAME, ast.Store())],
-                               ast.Tuple([ast.Constant(from_line), ast.Constant(to_line)], ast.Load()))
-
             if sys.version_info[0:2] >= (3,12):
+                # Using a constant Expr allows the compiler to optimize this to a NOP
+                mark = ast.Expr(ast.Constant(None))
                 for node in ast.walk(mark):
                     node.lineno = node.end_lineno = encode_branch(from_line, to_line)
                     # Leaving the columns unitialized can lead to invalid positions despite
                     # our use of ast.fix_missing_locations
                     node.col_offset = node.end_col_offset = -1
-            elif sys.version_info[0:2] == (3,11):
-                for node in ast.walk(mark):
-                    node.lineno = 0 # we ignore line 0, so this avoids generating extra line probes
             else:
-                for node in ast.walk(mark):
-                    node.lineno = from_line
+                mark = ast.Assign([ast.Name(BRANCH_NAME, ast.Store())],
+                                   ast.Tuple([ast.Constant(from_line), ast.Constant(to_line)], ast.Load()))
+                if sys.version_info[0:2] == (3,11):
+                    for node in ast.walk(mark):
+                        node.lineno = 0 # we ignore line 0, so this avoids generating extra line probes
+                else:
+                    for node in ast.walk(mark):
+                        node.lineno = from_line
 
             return [mark]
 
-        def visit_FunctionDef(self, node: Union[ast.AsyncFunctionDef, ast.FunctionDef]) -> ast.AST:
-            # Mark BRANCH_NAME global, so that our assignments are easier to find (only STORE_NAME/STORE_GLOBAL,
-            # but not STORE_FAST, etc.)
-            has_docstring = ast.get_docstring(node, clean=False) is not None
-            node.body.insert(1 if has_docstring else 0, ast.Global([BRANCH_NAME]))
-            super().generic_visit(node)
-            return node
+        if sys.version_info[0:2] < (3,12):
+            def visit_FunctionDef(self, node: Union[ast.AsyncFunctionDef, ast.FunctionDef]) -> ast.AST:
+                # Mark BRANCH_NAME global, so that our assignments are easier to find (only STORE_NAME/STORE_GLOBAL,
+                # but not STORE_FAST, etc.)
+                has_docstring = ast.get_docstring(node, clean=False) is not None
+                node.body.insert(1 if has_docstring else 0, ast.Global([BRANCH_NAME]))
+                super().generic_visit(node)
+                return node
 
-        def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef) -> ast.AST:
-            return self.visit_FunctionDef(node)
+            def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef) -> ast.AST:
+                return self.visit_FunctionDef(node)
 
         def _mark_branches(self, node: ast.AST) -> ast.AST:
             node.body = self._mark_branch(node.lineno, node.body[0].lineno) + node.body
